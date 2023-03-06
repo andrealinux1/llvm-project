fda196acf0ca93f45304c9d076ca71e109527c17
Andrea Gussoni <andreagus@rev.ng>
2023-03-06 13:56:13 +0100

    Change `RestructureClift` pass naming

--- mlir/lib/Dialect/Clift/Transforms/RestructureCliftPass.cpp
+++ mlir/lib/Dialect/Clift/Transforms/RestructureCliftPass.cpp
@@ -1,3 +1,15 @@
+//===- RestructureCliftPass.cpp - perform restructuring on Clift ----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Performs restructuring on Clift.
+//
+//===----------------------------------------------------------------------===//
+
 #include "mlir/Dialect/Clift/Transforms/Passes.h"
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
